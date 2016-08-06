23267a65973db77d4be79397e063e181f3f9c4f1
Loic Gudet <loic.gudet@gmail.com>
2016-08-06 18:15:31 +0200

    Element factory

--- resources/test_config.rb
+++ resources/test_config.rb
@@ -1,12 +1,12 @@
 
-store(Room.create('dining room', 0, 0) do
+store(RoomFactory.create('dining room', 0, 0) do
   wall(368, :right, width: 5).fill('blue')
   wall(570, :down, width: 5).fill('red')
   wall(368, :left, width: 5).fill('green')
-  wall(570, :up, name: 'dining_room_hall')
+  wall(570, :up, name: 'dining_room_hall').fill('blue')
 end)
 
-store(Room.create('hall', anchor: WallCache['dining_room_hall'].B2) do
+store(RoomFactory.create('hall', anchor: WallCache['dining_room_hall'].B2) do
   wall(335, :left, width: 5).fill('blue')
   wall(182, :down, name: 'hall_toilets')
   wall(117, :right)
@@ -14,24 +14,24 @@ store(Room.create('hall', anchor: WallCache['dining_room_hall'].B2) do
   wall(130, :right)
   wall(64, :down)
   wall(88, :right)
-  wall(182, :up)
+  wall(182, :up, name: 'dining_room_hall2')
 end)
 
-store(Room.create('toilets', *WallCache['hall_toilets'].B1.xy ) do
+store(RoomFactory.create('toilets', anchor: WallCache['hall_toilets'].B1 ) do
   wall(85, :down)
   wall(144, :left, name: 'toilets_bathroom')
   wall(85, :up, name: 'toilets_cmv')
   wall(144, :right, width: 5).fill('blue')
 end)
 
-store(Room.create('CMV', *WallCache['toilets_cmv'].B2.xy ) do
+store(RoomFactory.create('CMV', anchor: WallCache['toilets_cmv'].B2 ) do
   wall(85, :down)
   wall(49, :left)
   wall(85, :up, width: 5).fill('yellow')
   wall(49, :right, width: 5).fill('blue')
 end)
 
-store(Room.create('bathroom', *WallCache['toilets_bathroom'].B1.xy ) do
+store(RoomFactory.create('bathroom', anchor: WallCache['toilets_bathroom'].B1 ) do
   wall(195, :left)
   wall(194, :down, width: 5).fill('yellow')
   wall(175, :right, name: 'bathroom_bedroom')
@@ -39,8 +39,8 @@ store(Room.create('bathroom', *WallCache['toilets_bathroom'].B1.xy ) do
   wall(20, :right)
 end)
 
-store(Room.create('bedroom', *WallCache['bathroom_bedroom'].B1.xy ) do
-  wall(175, :right)
+store(RoomFactory.create('bedroom', anchor: WallCache['bathroom_bedroom'].B1 ) do
+  wall(175, :right, name: 'bathroom_bedroom2')
   wall(69, :up)
   wall(20, :right)
   wall(30, :up)
@@ -50,16 +50,16 @@ store(Room.create('bedroom', *WallCache['bathroom_bedroom'].B1.xy ) do
   wall(287, :up, width: 5).fill('yellow')
 end)
 
-store(Room.create('kitchen', *WallCache['bedroom_kitchen'].B2.xy) do
+store(RoomFactory.create('kitchen', anchor: WallCache['bedroom_kitchen'].B2) do
   wall(250, :right, width: 5).fill('green')
-  wall(386, :up)
+  wall(386, :up, name: 'kitchen')
   wall(190, :left)
   wall(60, :down)
   wall(60, :left)
   wall(318, :down)
 end)
 
-store(Room.create('gas line', *WallCache['bedroom_kitchen'].B1.xy) do
+store(RoomFactory.create('gas line', anchor: WallCache['bedroom_kitchen'].B1) do
   wall(58, :right)
   wall(58, :down)
   wall(58, :left)
