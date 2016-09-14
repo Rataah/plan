fbf9baa6746ab4bb9a91e39353d330ddc0509f96
Rataah <loic.gudet@gmail.com>
2016-09-14 20:02:46 +0200

    Window

--- lib/objects/window.rb
+++ lib/objects/window.rb
@@ -1,4 +1,23 @@
 module Plan
   class Window
+    attr_accessor :origin, :length
+
+    def initialize(origin, length)
+      @origin = origin
+      @length = length
+    end
+
+    def svg_elements(wall)
+      Plan.log.debug("Draw SVG elements for Window of wall: #{wall.name}")
+      window_a1 = wall.vertex_a1.translate(wall.angle, @origin)
+      window_b1 = wall.vertex_b1.translate(wall.angle, @origin)
+      window_a2 = window_a1.translate(wall.angle, @length)
+      window_b2 = window_b1.translate(wall.angle, @length)
+
+      [
+          SVGPolygon.new([window_a1, window_b1, window_b2, window_a2]).fill('white').stroke('black'),
+        SVGLine.new(wall.ab1.translate(wall.angle, @origin), wall.ab1.translate(wall.angle, @origin).translate(wall.angle, @length)).stroke('black')
+      ]
+    end
   end
 end
